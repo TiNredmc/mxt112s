d87c11f7057e63267166d8ca5fcecc8cc9b1c49a
Tin LetHax <thipok17@gmail.com>
2022-08-17 11:21:26 +0700

    Added object parser.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- software/MXT112S_arduino/MXT112S_arduino.ino
+++ software/MXT112S_arduino/MXT112S_arduino.ino
@@ -12,6 +12,16 @@
 // Number of elements in the Object Table
 uint8_t num_obj = 0;
 
+// Store address of each object
+uint16_t T5_addr = 0;
+uint16_t T9_addr = 0;
+uint16_t T100_addr = 0;
+
+// Store message size of each object
+uint8_t T5_msg_size = 0;
+uint8_t T9_msg_size = 0;
+uint8_t T100_msg_size = 0;
+
 // Read data from MXT112S
 void mxt_read(uint16_t addr, uint8_t *data, uint16_t len) {
 
@@ -57,7 +67,7 @@ void mxt_identify() {
   Serial.print("Version ");
   Serial.print((uint8_t)((buf[2] & 0xF0) << 4), DEC);
   Serial.print(".");
-  Serial.print((uint8_t)(buf[2] & 0x0F), DEC);
+  Serial.println((uint8_t)(buf[2] & 0x0F), DEC);
   Serial.print("Build Number 0x");
   Serial.println(buf[3], HEX);
 
@@ -68,6 +78,34 @@ void mxt_identify() {
 
   Serial.print("OJB table elements count");
   Serial.println(buf[6], DEC);
+
+  // maXTouch can have many object. But we specifically looking for T5 A.K.A Message Processor
+  // and T100 A.K.A Multitouch
+  //
+  for (uint8_t i = 0; i < num_obj; i ++) {
+    mxt_read(0x0007, buf, MXT_ID_BLK_SIZ - 1);
+
+    switch (buf[0]) {
+      case 5:// T5 message processor.
+        Serial.println("Found T5 object");
+        T5_addr = (buf[2] << 8) | buf[1];
+        T5_msg_size = buf[3] + 1;
+        break;
+
+      case 9:// T9 Multi touch (Assuming the maXTouch S series have this instead of T100 from U series).
+        Serial.println("Found T9 object");
+        T9_addr = (buf[2] << 8) | buf[1];
+        T9_msg_size = buf[3] + 1;
+        break;
+
+      case 100:// T100 Multi touch
+        Serial.println("Found T100 object");
+        T100_addr = (buf[2] << 8) | buf[1];
+        T100_msg_size = buf[3] + 1;
+        break;
+    }
+    
+  }
 
 }
 
