d81144781a31d17b090f42d612fc56c5ccc711bd
Tin LetHax <thipok17@gmail.com>
2022-09-08 11:01:26 +0700

    [W.I.P] A bunch of updates.
    - mxt_read() and mxt_write() should function properly.
    - mxt_identify() :
    	- now using genral purpose MXT_BUF array.
    	- Added CRC42 dummy read and T7 Power config to set in Free running mode.
    	- Report X and Y range over Serial port.
    - mxt_report_t9() sould function properly on touch.
    
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- software/MXT112S_arduino/MXT112S_arduino.ino
+++ software/MXT112S_arduino/MXT112S_arduino.ino
@@ -14,26 +14,21 @@
 
 // Number of elements in the Object Table
 uint8_t num_obj = 0;
+// Object query address.
+uint16_t obj_addr_cnt = 0;
 
+/* Register map*/
 // Store address of each object.
 uint16_t T5_addr = 0;
+uint16_t T7_addr = 0;
 uint16_t T9_addr = 0;
-//uint16_t T100_addr = 0;
 
-// Store message size of each object.
-uint8_t T5_msg_size = 0;
-uint8_t T9_msg_size = 0;
-//uint8_t T100_msg_size = 0;
+// TODO : properly build report ID map.
+// Store min and max report ID of T9
+uint8_t T9_report_id_min = 0;
+uint8_t T9_report_id_max = 0;
 
-// Store number of report instances of each report.
-uint8_t T9_instances = 0;
-//uint8_t T100_instances = 0;
-
-// Store number of report ID per instance.
-uint16_t T9_report_cnt = 0;
-//uint16_t T100_report_cnt = 0;
-
-// Genral purpose buffer array
+// Genral purpose buffer array.
 uint8_t MXT_BUF[256];
 
 uint16_t xpos, ypos;
@@ -43,8 +38,8 @@ uint8_t amplitude;
 void mxt_read(uint16_t addr, uint8_t *data, uint16_t len) {
 
   Wire.beginTransmission(MXT_ADDR);// Begin TX
-  Wire.write((uint8_t)(addr >> 8));// Write LSB byte address to MXT112S.
-  Wire.write((uint8_t)addr);// Write MSB byte address to MXT112S.
+  Wire.write((uint8_t)addr);// Write LSB byte address to MXT112S.
+  Wire.write((uint8_t)(addr >> 8));// Write MSB byte address to MXT112S.
 
   Wire.endTransmission();
 
@@ -59,8 +54,8 @@ void mxt_read(uint16_t addr, uint8_t *data, uint16_t len) {
 void mxt_write(uint16_t addr, uint8_t *data, uint8_t len) {
 
   Wire.beginTransmission(MXT_ADDR);// Begin tx
-  Wire.write((uint8_t)(addr >> 8));// Write LSB byte address to MXT112S.
-  Wire.write((uint8_t)addr);// Write MSB byte address to MXT112S.
+  Wire.write((uint8_t)addr);// Write LSB byte address to MXT112S.
+  Wire.write((uint8_t)(addr >> 8));// Write MSB byte address to MXT112S.
 
   while (len--)
     Wire.write(*data++);
@@ -71,91 +66,102 @@ void mxt_write(uint16_t addr, uint8_t *data, uint8_t len) {
 
 // Query basic informations of MXT112S
 void mxt_identify() {
-  uint8_t buf[MXT_ID_BLK_SIZ] = {0};
+  mxt_read(0x0000, MXT_BUF, MXT_ID_BLK_SIZ);
 
-  mxt_read(0x0000, buf, MXT_ID_BLK_SIZ);
-
-  num_obj = buf[6];// save the object table elements count.
+  num_obj = MXT_BUF[6];// save the object table elements count.
 
   Serial.print("Family ID 0x");
-  Serial.println(buf[0], HEX);
+  Serial.println(MXT_BUF[0], HEX);
   Serial.print("Variant ID 0x");
-  Serial.println(buf[1], HEX);
+  Serial.println(MXT_BUF[1], HEX);
   Serial.print("Version ");
-  Serial.print((uint8_t)((buf[2] & 0xF0) << 4), DEC);
+  Serial.print((uint8_t)((MXT_BUF[2] & 0xF0) >> 4), DEC);
   Serial.print(".");
-  Serial.println((uint8_t)(buf[2] & 0x0F), DEC);
+  Serial.println((uint8_t)(MXT_BUF[2] & 0x0F), DEC);
   Serial.print("Build Number 0x");
-  Serial.println(buf[3], HEX);
+  Serial.println(MXT_BUF[3], HEX);
 
   Serial.print("X channels :");
-  Serial.println(buf[4], DEC);
+  Serial.println(MXT_BUF[4], DEC);
   Serial.print("Y channels :");
-  Serial.println(buf[5]);
+  Serial.println(MXT_BUF[5], DEC);
 
   Serial.print("OJB table elements count");
-  Serial.println(buf[6], DEC);
+  Serial.println(num_obj, DEC);
 
   // maXTouch can have many object. But we specifically looking for T5 A.K.A Message Processor
   // and T100 A.K.A Multitouch
   //
+  // maXTouch can have many object. But we specifically looking for T5 A.K.A Message Processor
+  // and T9 Multitouch object
+ 
   for (uint8_t i = 0; i < num_obj; i ++) {
-    mxt_read(0x0007, buf, MXT_ID_BLK_SIZ - 1);
+  mxt_read(obj_addr_cnt, MXT_BUF, MXT_ID_BLK_SIZ - 1);
 
-    switch (buf[0]) {
-      case 5:// T5 message processor.
-        Serial.println("Found T5 object");
-        T5_addr = (buf[2] << 8) | buf[1];
-        T5_msg_size = buf[3] + 1;
-        break;
+  switch (MXT_BUF[0]) {
+    case 5:// T5 message processor.
+      Serial.print("Found T5 object\n");
+      T5_addr = (MXT_BUF[2] << 8) | MXT_BUF[1];
+      Serial.print("T5 object address : 0x");
+      Serial.println(T5_addr, HEX);
+      break;
 
-      case 9:// T9 Multi touch (Assuming the maXTouch S series have this instead of T100 from U series).
-        Serial.println("Found T9 object");
-        T9_addr = (buf[2] << 8) | buf[1];
-        T9_msg_size = buf[3] + 1;
-        T9_instances = buf[4] + 1;
-        T9_report_cnt = (buf[4] + 1) * buf[5];
-        break;
+    case 7:// T7 power config stuffs.
+      Serial.print("Found T7 Object\n");
+      T7_addr = (MXT_BUF[2] << 8) | MXT_BUF[1];
+      break;
 
-//      case 100:// T100 Multi touch
-//        Serial.println("Found T100 object");
-//        T100_addr = (buf[2] << 8) | buf[1];
-//        T100_msg_size = buf[3] + 1;
-//        T100_instances = buf[4] + 1;
-//        T100_report_cnt = (buf[4] + 1) * buf[5];
-//        break;
-    }
-    
+    case 9:// T9 Multi touch (Assuming the maXTouch S series have this instead of T100 from U series).
+      Serial.print("Found T9 object\n");
+      T9_addr = (MXT_BUF[2] << 8) | MXT_BUF[1];
+      Serial.print("T9 object address : 0x");
+      Serial.println(T9_addr, HEX);
+
+      // TODO : properly build report ID map.
+      //T9_report_id_min = MXT_BUF[5];
+      //T9_report_id_max = MXT_BUF[5] + MXT_BUF[4]; 
+      break;
+
+  }
+  
+  obj_addr_cnt += 6;// move on to next object.
   }
 
+  // Dummy read CRC. We don't actually use it.
+  mxt_read(obj_addr_cnt, MXT_BUF, 3);
+
+  // Get X,Y range
+  mxt_read(T9_addr + 18, MXT_BUF, 4);
+  Serial.print("X range : ");
+  Serial.println((MXT_BUF[1] << 8) | MXT_BUF[0], DEC);
+  Serial.print("Y range : ");
+  Serial.println((MXT_BUF[3] << 8) | MXT_BUF[2], DEC);
+
+  // Set T7 into Free-running mode
+  mxt_read(T7_addr, MXT_BUF, 3);
+  MXT_BUF[0] = 255;
+  MXT_BUF[1] = 255;
+  mxt_write(T7_addr, MXT_BUF, 3);
 }
 
 // Report Multi touch from T9 object.
 // But data is read from T5 message object.
-// Since T9 can report Upto 10 fingers. 
-// The report ID can be more than 1 Report ID.
-// Starting from 0x09 (T9 base report ID).
 void mxt_report_t9(){
   if(digitalRead(MXT_INT) == 0){
-    mxt_read(T5_addr, MXT_BUF, T9_msg_size);
-  
-    if(MXT_BUF[0] != 9){// If we didn't get T9 report (check for the T9 report ID).
-      // Just ignore the interrupt.
-      return;      
-    }
-
-    // Check status from first byte.
-    if(MXT_BUF[1] & 0x02)// If touch has been suppressed by Grip or Face (imagine put your phone to your ear).
-     // Just ignore
-      return;
-
+    mxt_read(T5_addr, MXT_BUF, 16);
+    Serial.write(0x0C);// clear terminal
     // Convert 2 bytes to 16bit uint X and Y position 
-    xpos = (MXT_BUF[0x02] << 4) | ((MXT_BUF[0x04] >> 4) & 0x0F);
-    ypos = (MXT_BUF[0x03] << 4) | (MXT_BUF[0x04] & 0x0F);
+    xpos = (MXT_BUF[2] << 4) | ((MXT_BUF[4] >> 4) & 0x0F);
+    ypos = (MXT_BUF[3] << 2) | (MXT_BUF[4] & 0x0F);
 
-    // Check for touch amplitude (Pressure?)
-    if(MXT_BUF[1] & 0x04)
-      amplitude =  MXT_BUF[0x06];
+    amplitude =  MXT_BUF[6];
+
+    Serial.print("X pos : ");
+    Serial.println(xpos, DEC);
+    Serial.print("Y pos : ");
+    Serial.println(ypos, DEC);
+    Serial.print("Pressure : ");
+    Serial.println(amplitude, DEC);    
   }
 }
 
@@ -166,12 +172,12 @@ void setup() {
   Wire.begin();// Init I2C
 
   pinMode(MXT_INT, INPUT_PULLUP);
-
-  delay(100);
   mxt_identify();
+  delay(100);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  mxt_report_t9();
+  delay(10);// throttle down a bit. 
 }
